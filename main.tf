1de46e433f18b581ef70ca4def9c40b78b52e6c9
metal <metalheadkrn@gmail.com>
2026-02-13 11:24:37 +0530

    Making changes to miscellaneous code supporting DRY concept

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "ec2" {
   source = "./modules/ec2"
   for_each = var.tool
   tool = each.key
-  instance-type = each.value["instance_type"]
+  instance_type = each.value["instance_type"]
 }
 
