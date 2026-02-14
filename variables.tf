891f09aadeff3f84c2b82e35af9cb5c5fa0a9628
metal <metalheadkrn@gmail.com>
2026-02-14 21:40:32 +0530

    Creating Pipelines for all the tools

--- variables.tf
+++ variables.tf
@@ -4,6 +4,20 @@ variable "tool" {
       instance_type = "t3.large"
       port          = 9000
       priority      = 100
-    },
+    }
   }
+
+  elasticsearch = {
+    instance_type = "m6in.large"
+    port          = 9200
+    priority      = 101
+  }
+
+  prometheus = {
+    instance_type = "t3.small"
+    port          = 9090
+    priority      = 102
+  }
+
 }
+
