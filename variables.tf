831458338a8ad85c84f684faad46d3f32884c5ca
metal <metalheadkrn@gmail.com>
2026-02-15 08:35:38 +0530

    Updates variables code block syntax

--- variables.tf
+++ variables.tf
@@ -5,18 +5,20 @@ variable "tool" {
       port          = 9000
       priority      = 100
     }
-  }
 
-  elasticsearch = {
-    instance_type = "m6in.large"
-    port          = 9200
-    priority      = 101
-  }
 
-  prometheus = {
-    instance_type = "t3.small"
-    port          = 9090
-    priority      = 102
+    elasticsearch = {
+      instance_type = "m6in.large"
+      port          = 9200
+      priority      = 101
+    }
+
+    prometheus = {
+      instance_type = "t3.small"
+      port          = 9090
+      priority      = 102
+    }
+
   }
 
 }
