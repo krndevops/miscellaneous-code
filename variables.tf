ca360998af0faeaf99aeaf7478fe5814bb33caa8
metal <metalheadkrn@gmail.com>
2026-04-08 12:59:23 +0530

    Disable prometheus stack from ec2 servers, we will have this in EKS directly

--- variables.tf
+++ variables.tf
@@ -17,29 +17,29 @@ variable "tool" {
 
     }
 
-    prometheus = {
-      instance_type = "t3.small"
-      port          = 9090
-      priority      = 102
-      policy_list   = ["ec2:Describe*"]
-
-    }
-
-    grafana = {
-      instance_type = "t3.small"
-      port          = 3000
-      priority      = 103
-      policy_list   = []
-
-    }
-
-    alertmanager = {
-      instance_type = "t3.small"
-      port          = 9093
-      priority      = 104
-      policy_list   = []
-
-    }
+    # prometheus = {
+    #   instance_type = "t3.small"
+    #   port          = 9090
+    #   priority      = 102
+    #   policy_list   = ["ec2:Describe*"]
+    #
+    # }
+    #
+    # grafana = {
+    #   instance_type = "t3.small"
+    #   port          = 3000
+    #   priority      = 103
+    #   policy_list   = []
+    #
+    # }
+    #
+    # alertmanager = {
+    #   instance_type = "t3.small"
+    #   port          = 9093
+    #   priority      = 104
+    #   policy_list   = []
+    #
+    # }
 
     artifactory = {
       instance_type = "t3.large"
